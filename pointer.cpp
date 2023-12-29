b7fc74ca8bd554a8ed6e61e7474e655d513b1405
Ivan40n9 <dongivan6114@gmail.com>
2023-12-29 22:57:26 +0800

    new创建数和数组

--- pointer.cpp
+++ pointer.cpp
@@ -7,4 +7,13 @@ int main(){
     *pd = 10000001.0;
     cout<<"double value = "<<*pd<<":location = "<< pd <<endl;
     cout<<"location of pointer pd:"<< &pd << endl;
+    delete pd;
+    int* psome = new int [10];
+    for(int i = 0; i < 10; i++){
+        psome[i] = i;
+    }
+    for(int i = 0; i < 10; i++){
+        cout<<psome[i]<<endl;
+    }
+        delete [] psome;
 }

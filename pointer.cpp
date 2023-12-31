8f3e33ce82382c677d68d7f692d5b4a71173b555
Ivan40n9 <dongivan6114@gmail.com>
2023-12-31 21:17:45 +0800

    指针字符串

--- pointer.cpp
+++ pointer.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
 #include <cstdio>
 
-int main(){
+int main()
+{
     using namespace std;
-    double* pd = new double;
+    double *pd = new double;
     *pd = 10000001.0;
-    cout<<"double value = "<<*pd<<":location = "<< pd <<endl;
-    cout<<"location of pointer pd:"<< &pd << endl;
+    cout << "double value = " << *pd << ":location = " << pd << endl;
+    cout << "location of pointer pd:" << &pd << endl;
     delete pd;
-    int* psome = new int [10];
-    for(int i = 0; i < 10; i++){
+    int *psome = new int[10];
+    for (int i = 0; i < 10; i++)
+    {
         psome[i] = i;
     }
-    for(int i = 0; i < 10; i++){
-        cout<<psome[i]<<endl;
+    for (int i = 0; i < 10; i++)
+    {
+        cout << psome[i] << endl;
     }
-        delete [] psome;
+    delete[] psome;
+    // 指针和字符串
+    char flower[10] = "rose";
+    cout << flower << "s are red";
 }
